d9a18b921459bf46abb39a190cfc1ea0853d59c6
oniricoGH <adrian.gafo@gmail.com>
2018-08-15 15:17:24 +0200

    Minor changes on assignment1

--- AngularJS/course/assignment1-solution/js/lunch.js
+++ AngularJS/course/assignment1-solution/js/lunch.js
@@ -1,41 +1,51 @@
-//(function (){
-//    
-//'use strict';
-//    
-//angular.module('LunchCheck',[])
-//    .controller('LunchCheckController', LunchCheckController);
-//
-//LunchCheckController.$inject = ['$scope'];
-//    
-//function LunchCheckController($scope) {
-//    // ALWAYS init variables, otherwise is undefined
-//    $scope.outputMessage = "";
-//    $scope.lunchItems = [];
-//        
-//    $scope.checkLunch = function () {
-//        popEmptyElements();
-//        if($scope.lunchItems.length>3)
-//            $scope.outputMessage = "too much!";
-//        else if($scope.lunchItems.length>0)
-//            $scope.outputMessage = "enjoy!";
-//        else
-//            $scope.outputMessage = "";
-//    }
-//    
-//    function popEmptyElements() {
-//        for(var i=0;i<$scope.lunchItems.length;){
-//            // remove spaces and if length==0 then no word
-//            if (!$scope.lunchItems[i].replace(/\s/g, '').length) {
-//                // the item is deleted so all elements decreases the index automatically, so not necessary increase for index
-//                $scope.lunchItems.splice(i,1); 
-//            }
-//            else
-//                // manual increase index
-//                i++;
-//        }       
-//    }
-//}
-//})();
+(function (){
+    
+'use strict';
+    
+angular.module('LunchCheck',[])
+    .controller('LunchCheckController', LunchCheckController);
+
+LunchCheckController.$inject = ['$scope'];
+    
+function LunchCheckController($scope) {
+    // ALWAYS init variables, otherwise is undefined
+    $scope.outputMessage = "Please enter data first (spaces are not considered as list elements)";
+    $scope.lunchItems = [];
+    $scope.outMsgStyle = {color: 'red'};
+    $scope.inputStyle = {borderColor: 'red'};
+    
+    
+    $scope.checkLunch = function () {
+        popEmptyElements();
+        
+        if($scope.lunchItems.length>0) {
+            if($scope.lunchItems.length>3)
+                $scope.outputMessage = "TOO MUCH FOOD!";
+            else
+                $scope.outputMessage = "ENJOY!";
+            $scope.outMsgStyle = {color: 'green'};
+            $scope.inputStyle = {borderColor: 'green'};
+        }else {
+            $scope.outputMessage = "Please enter data first (spaces are not considered as list elements)";
+            $scope.outMsgStyle = {color: 'red'};
+            $scope.inputStyle = {borderColor: 'red'};
+        }
+    }
+    
+    function popEmptyElements() {
+        for(var i=0;i<$scope.lunchItems.length;){
+            // remove spaces and if length==0 then no word
+            if (!$scope.lunchItems[i].replace(/\s/g, '').length) {
+                // the item is deleted so all elements decreases the index automatically, so not necessary increase for index
+                $scope.lunchItems.splice(i,1); 
+            }
+            else
+                // manual increase index
+                i++;
+        }       
+    }
+}
+})();
 
 
-!function(){"use strict";function t(t){t.outputMessage="",t.lunchItems=[],t.checkLunch=function(){!function(){for(var e=0;e<t.lunchItems.length;)t.lunchItems[e].replace(/\s/g,"").length?e++:t.lunchItems.splice(e,1)}(),t.lunchItems.length>3?t.outputMessage="too much!":t.lunchItems.length>0?t.outputMessage="enjoy!":t.outputMessage=""}}angular.module("LunchCheck",[]).controller("LunchCheckController",t),t.$inject=["$scope"]}();
+//!function(){"use strict";function e(e){e.outputMessage="Please enter data first (spaces are not considered as list elements)",e.lunchItems=[],e.outMsgStyle={color:"red"},e.inputStyle={borderColor:"red"},e.checkLunch=function(){!function(){for(var t=0;t<e.lunchItems.length;)e.lunchItems[t].replace(/\s/g,"").length?t++:e.lunchItems.splice(t,1)}(),e.lunchItems.length>0?(e.lunchItems.length>3?e.outputMessage="TOO MUCH FOOD!":e.outputMessage="ENJOY!",e.outMsgStyle={color:"green"},e.inputStyle={borderColor:"green"}):(e.outputMessage="Please enter data first (spaces are not considered as list elements)",e.outMsgStyle={color:"red"},e.inputStyle={borderColor:"red"})}}angular.module("LunchCheck",[]).controller("LunchCheckController",e),e.$inject=["$scope"]}();

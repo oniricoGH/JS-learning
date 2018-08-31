a3043a9990c59dd1f312a67d44fe9404acd7fbfb
oniricoGH <adrian.gafo@gmail.com>
2018-08-31 13:27:42 +0200

    Ass#3 Working with simple directive template

---
+++ AngularJS/course/assignment3/assignment3-solution/js/app.js
@@ -0,0 +1,82 @@
+(function (){
+    
+'use strict';
+    
+angular.module('NarrowItDownApp',[])
+.controller('NarrowItDownController', NarrowItDownController)
+.service('MenuSearchService', MenuSearchService)
+.constant('MenuUrl', "https://davids-restaurant.herokuapp.com/menu_items.json")
+.directive('foundItems', FoundItemsDirective);
+    
+function FoundItemsDirective() {
+    var ddo = {
+        template: '{{ item.name }} ({{item.short_name}}): {{ item.description}}',
+        
+    };
+    
+    return ddo;
+}
+
+
+NarrowItDownController.$inject = ['MenuSearchService'];
+function NarrowItDownController (MenuSearchService){
+    // required for controller as syntax?
+    var ctrl = this;
+    // keyword to filter result array
+    ctrl.keyWord = "";
+    // array with matched elements
+    ctrl.found = [];
+    
+    // function called from UI that checks if there is a keyword and starts the data provider service     
+    ctrl.showMenuList = function () {
+       if(ctrl.keyWord===""){
+           ctrl.found = [];
+           console.log("Keyword not found!");
+        }
+        else {
+            ctrl.found = [];
+            var promise = MenuSearchService.getMatchedMenuItems();
+            promise.then(httpSuccess,httpError);
+        }
+        
+    }
+    
+    // the argument index comes from the directive
+    ctrl.removeItem = function (itemIndex){
+        console.log("Next to be deleted: ", this.found[itemIndex], "index: ", itemIndex);
+        this.found.splice(itemIndex, 1);
+        console.log("Current array", this.found);
+    }
+    
+    function httpSuccess(response){
+        var objArray=response.data.menu_items;
+
+        console.log("Success in HTTP request");
+        console.log("objs: ", objArray);
+        console.log("keyword: ", ctrl.keyWord);
+
+        for (var i=0;i<objArray.length;i++) {                                   if(objArray[i].description.search(ctrl.keyWord)!==-1){
+                ctrl.found.push(objArray[i]);
+            }
+        }
+        
+        console.log("found items size: ", ctrl.found.length, " , found items array: ", ctrl.found);
+    }
+    
+    function httpError(response){
+        console.log("Error in HTTP request, status: ", response.status);
+    }
+    
+}
+    
+MenuSearchService.$inject = ['$http', 'MenuUrl'];
+function MenuSearchService($http, MenuUrl){
+    var srvc = this;
+    
+    // main function to start the http process
+    srvc.getMatchedMenuItems = function(keyword){
+        return $http({ url: MenuUrl});
+    }  
+}
+    
+})();
